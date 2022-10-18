6629f1cbb9c8229ada6da70a68dd777257e641d6
prosper <edwardprosper001@gmail.com>
2022-10-19 00:29:41 +0100

    updates

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+
 class LoginForm(forms.Form):
-username = forms.CharField()
-password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
