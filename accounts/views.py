7c402332cde8fb8a00d11a0f44690b8bad844891
prosper <edwardprosper001@gmail.com>
2022-10-19 10:02:52 +0100

    updates

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm, UserEditForm, ProfileEditForm
+from .forms import LoginForm, UserEditForm, ProfileEditForm, UserRegistrationForm
+from .models import Profile
+from django.contrib import messages
 
 
 def user_login(request):
@@ -31,52 +33,34 @@ def dashboard(request):
     return render(request, 'accounts/dashboard.html', {'section': 'dashboard'})
 
 
-from .models import Profile
+
 # ...
 def register(request):
-if request.method == 'POST':
-user_form = UserRegistrationForm(request.POST)
-if user_form.is_valid():
-# Create a new user object but avoid saving it yet
-new_user = user_form.save(commit=False)
-# Set the chosen password
-new_user.set_password(
-user_form.cleaned_data['password'])
-# Save the User object
-new_user.save()
-# Create the user profile
-Profile.objects.create(user=new_user)
-return render(request,
-'account/register_done.html',
-{'new_user': new_user})
-else:
-user_form = UserRegistrationForm()
-return render(request,
-'account/register.html',
-{'user_form': user_form})
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            Profile.objects.create(user=new_user)
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'accounts/register.html', {'user_form': user_form})
 
 
 
 def edit(request):
-if request.method == 'POST':
-user_form = UserEditForm(instance=request.user,
-data=request.POST)
-profile_form = ProfileEditForm(
-instance=request.user.profile,
-data=request.POST,
-files=request.FILES)
-if user_form.is_valid() and profile_form.is_valid():
-user_form.save()
-profile_form.save()
-messages.success(request, 'Profile updated '\
-'successfully')
-else:
-messages.error(request, 'Error updating your profile')
-else:
-user_form = UserEditForm(instance=request.user)
-profile_form = ProfileEditForm(
-instance=request.user.profile)
-return render(request,
-'account/edit.html',
-{'user_form': user_form,
-'profile_form': profile_form})
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+        return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})
