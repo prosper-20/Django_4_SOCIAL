ecb2bb8aeda34abbf1555804b84734cecc6192d3
prosper <edwardprosper001@gmail.com>
2022-10-20 11:00:02 +0100

    updates

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -143,3 +143,18 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
