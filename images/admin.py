e6f6533d9be71bbe0953c77d386f7ba1413a41ff
prosper <edwardprosper001@gmail.com>
2022-10-24 10:26:53 +0100

    updates

--- images/admin.py
+++ images/admin.py
@@ -5,3 +5,4 @@ from .models import Image
 class ImageAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'image', 'created']
     list_filter = ['created']
+    prepopulated_fields = {"slug": ("title",)}
